1337cb2d1e228acb01ee1b8d0c1147e0afb30742
cuiyang <cuiyang@100doc.com.cn>
2024-12-30 17:12:00 +0800

    添加security

--- src/main/java/com/example/demo/security/MyUserDetailsService.java
+++ src/main/java/com/example/demo/security/MyUserDetailsService.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import jakarta.annotation.Resource;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -19,20 +21,17 @@ public class MyUserDetailsService implements UserDetailsService {
     @Resource
     private PasswordEncoder passwordEncoder;
 
-    private static final Collection<GrantedAuthority> authorities = new ArrayList<>();
-
-    static {
-        GrantedAuthority defaultRole = new SimpleGrantedAuthority("common");
-        GrantedAuthority xxlJobRole = new SimpleGrantedAuthority("xxl-job");
-        authorities.add(defaultRole);
-        authorities.add(xxlJobRole);
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws AuthenticationException {
         CustomUserDetails userDetails;
         // 这里模拟从数据库中获取用户信息
         if (username.equals("admin")) {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("common"));
+            authorities.add(new SimpleGrantedAuthority("xxl-job"));
+
+            List<GrantedAuthority> myAuth = AuthorityUtils.createAuthorityList("test-user");
+            authorities.addAll(myAuth);
             //这里的admin用户拥有common和xxl-job两个权限
             userDetails = new CustomUserDetails("admin", passwordEncoder.encode("123456"), authorities);
             userDetails.setAge(25);
